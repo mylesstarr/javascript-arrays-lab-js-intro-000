e83db50b7f3c787f52eede1b40cf8d5eb55cada6
Myles Starr <mylesstarr@gmail.com>
2021-03-24 02:46:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,17 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   return [...kittens, name]
 }
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  newKittens = kittens.slice(-1)
+  return newKittens
+}
+
+function removeFirstKitten() {
+  newKittens2 = kittens.slice(0)
+  return newKittens2
+}
